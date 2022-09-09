86780d6408bba5d9a2467ebfa5bbcc7453db2869
Nitin Kadam <nitin.learning213@gmail.com>
2022-09-09 16:57:33 +0530

    Updates

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -4,20 +4,14 @@ import Title from "./Title";
 import Description from "./Description";
 import Links from "./Links";
 import Countdown from "./Countdown";
-import Subscribe from "./Subscribe";
 import logo from "../images/gear-with-holes.svg";
-import xmark from "../images/x-mark.svg";
-import check from "../images/check-mark.svg";
-import facebook from "../images/fbookicon.svg";
-import instagram from "../images/instaicon.svg";
-import youtube from "../images/youtubeicon.svg";
-import twitter from "../images/twittericon.svg";
+import telegram from "../images/fbookicon.svg";
 import "../styles/ComingSoon.css";
 
 class ComingSoon extends Component {
   state = {
     countdown: {
-      countdownDate: "2020-12-31 00:00:00"
+      countdownDate:  "2022-10-15 18:00:00",
     },
     logo: {
       alt: "Spinning Gear",
@@ -25,82 +19,19 @@ class ComingSoon extends Component {
       spinSpeed: "slow"
     },
     title: {
-      text: "Coming Soon!"
+      text: "लवकरच आपल्या सेवेत"
     },
     description: {
-      text:
-        "The Coding From Null course platform will be up and running shortly. Please subscribe to our newsletter below to receive updates when new course material is available."
-    },
-    subscribe: {
-      placeholder: "Enter Email Address",
-      buttonText: "Submit"
+      text: "धनगर शुभ विवाह",
+      slogan: "धनगर समाजासाठी महाराष्ट्रातील नंबर १ विवाह संस्था",
     },
     links: [
       {
-        url: "https://www.facebook.com/groups/CodingFromNull",
-        logo: facebook,
-        text: "Join"
+        url: "https://t.me/dhangarshubhvivah",
+        logo: telegram,
+        text: ""
       },
-      {
-        url: "https://www.instagram.com/codingfromnull",
-        logo: instagram,
-        text: "Follow"
-      },
-      {
-        url: "https://www.youtube.com/channel/UC9Psp9-p9jgHfDBReAAcZ3w",
-        logo: youtube,
-        text: "Watch"
-      },
-      {
-        url: "https://www.twitter.com/CodingFromNull",
-        logo: twitter,
-        text: "Tweet"
-      }
     ],
-    notification: {
-      src: "",
-      alt: "",
-      message: "",
-      visible: false,
-      level: ""
-    }
-  };
-
-  configureNotification = obj => {
-    const notification = { ...this.state.notification };
-    notification.message = obj.body.msg;
-    if (obj.status === 200) {
-      notification.src = check
-      notification.alt = "Check Mark"
-      notification.level = "success"
-    } else {
-      notification.src = xmark
-      notification.alt = "X Mark"
-      notification.level = "error"
-    }
-    this.setState({ notification });
-  };
-
-  showNotification = () => {
-    const notification = { ...this.state.notification };
-    notification.visible = true;
-    this.setState({ notification }, () => {
-      setTimeout(() => {
-        notification.visible = false;
-        this.setState({ notification });
-      }, 3000);
-    });
-  };
-
-  changeLogoSpeed = () => {
-    const logo = { ...this.state.logo };
-    logo.spinSpeed = "fast";
-    this.setState({ logo }, () => {
-      setTimeout(() => {
-        logo.spinSpeed = "slow";
-        this.setState({ logo });
-      }, 1000);
-    });
   };
 
   render() {
@@ -108,10 +39,8 @@ class ComingSoon extends Component {
       title,
       description,
       logo,
-      subscribe,
       links,
       countdown,
-      notification
     } = this.state;
 
     return (
@@ -121,18 +50,7 @@ class ComingSoon extends Component {
         <Title text={title.text} />
         <Description
           text={description.text}
-          src={notification.src}
-          alt={notification.alt}
-          message={notification.message}
-          visible={notification.visible}
-          level={notification.level}
-        />
-        <Subscribe
-          placeholder={subscribe.placeholder}
-          buttonText={subscribe.buttonText}
-          changeLogoSpeed={this.changeLogoSpeed}
-          configureNotification={this.configureNotification}
-          showNotification={this.showNotification}
+          slogan={description.slogan}
         />
         <Links links={links} />
       </div>
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,29 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Notification from "./Notification";
 
 import "../styles/Description.css";
 
-const Description = ({ text, src, alt, message, visible, level }) => (
+const Description = ({ text, slogan }) => (
   <div className="description">
-    <p className="description-text">{text}</p>
-    <Notification
-      src={src}
-      alt={alt}
-      message={message}
-      visible={visible}
-      level={level}
-    />
+    <p className="text">{text}</p>
+    <p className="slogan">{slogan}</p>
   </div>
 );
 
 Description.propTypes = {
   text: PropTypes.string.isRequired,
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-  message: PropTypes.string.isRequired,
-  visible: PropTypes.bool.isRequired,
-  level: PropTypes.string.isRequired
+  slogan: PropTypes.string.isRequired,
 };
 
 export default Description;
